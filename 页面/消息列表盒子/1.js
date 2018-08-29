6d6ec8a4b9f3d0a41d9c91d85abc23d381a24cfe
章志成 <33870333+whitehairpin1223@users.noreply.github.com>
2018-08-29 19:47:13 +0800

    列表消息盒子

--- 页面/消息列表盒子/1.js
+++ 页面/消息列表盒子/1.js
@@ -1,23 +1,22 @@
 var tabs = document.querySelectorAll('.info-box li a');
 var panels = document.querySelectorAll('.info-box article');
 
-for(i = 0; i < tabs.length; i++) {
+for (i = 0; i < tabs.length; i++) {
   var tab = tabs[i];
   setTabHandler(tab, i);
 }
 
 function setTabHandler(tab, tabPos) {
-  tab.onclick = function() {
-    for(i = 0; i < tabs.length; i++) {
-      if(tabs[i].getAttribute('class')) {
+  tab.onclick = function () {
+    for (i = 0; i < tabs.length; i++) {
+      if (tabs[i].getAttribute('class')) {
         tabs[i].removeAttribute('class');
       }
     }
-
     tab.setAttribute('class', 'active');
 
-    for(i = 0; i < panels.length; i++) {
-      if(panels[i].getAttribute('class')) {
+    for (i = 0; i < panels.length; i++) {
+      if (panels[i].getAttribute('class')) {
         panels[i].removeAttribute('class');
       }
     }
